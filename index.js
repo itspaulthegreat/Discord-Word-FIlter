536248d22a13c27f95919c161e2b70c3be743ad4
itspaulthegreat <arindam.8599@gmail.com>
2021-02-15 10:48:51 +0530

    this is it

---
+++ index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+
+//Don't touch from here down unless you know partially what you're doing
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+
+client.login(config.BOT_TOKEN).catch(console.error);
+client.on('ready', () => {
+  console.log('Now Online, ready to filter!');
+});
+
+
+client.on('message', message => {
+  if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
+    message.delete()
+    const embed = new Discord.RichEmbed() //Ver 11.5.1 of Discord.js
+  .setTitle("BlackListed Word")
+  .setDescription("This is a blacklisted word")
+  .setTimestamp()
+  ms = message.channel.send(embed).then(msg => {
+    msg.delete(10000)
+  })
+  .catch("error");
+
+  }})
