2330629592935215c4838db093cd43a994e3f2c6
itspaulthegreat <arindam.8599@gmail.com>
2021-02-15 11:24:14 +0530

    up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 //const token = require("./token.json");
-
+console.log(process.env.token)
 client.login(process.env.token).catch(console.error);
 client.on('ready', () => {
   console.log('Now Online, ready to filter!');
